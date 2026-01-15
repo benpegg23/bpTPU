9f514508e66729ec3e4faf8860ed58b3a77f0a20
benpegg23 <benpegg23@gmail.com>
2026-01-14 23:55:07 -0800

    progress on skew buffer, created shift_register

---
+++ bpTPU.srcs/sources_1/new/shift_register.sv
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Simple synchronus shift register with a shift enable and an active-low reset 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module shift_register # (
+    parameter int INPUT_WIDTH = 8, 
+    parameter int INPUT_DEPTH = 1
+) (
+    input logic clk,
+    input logic rst_n, 
+    input logic shift_enable,
+    input logic [INPUT_WIDTH-1:0] in_data,
+    output logic [INPUT_WIDTH-1:0] out_data
+);
+
+// data shifted in -> [0], [1], ..., [INPUT_DEPTH - 1] -> data shifted out
+
+logic [INPUT_WIDTH-1:0] shift_reg [INPUT_DEPTH-1:0];
+integer i;
+always_ff @ (posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+        out_data <= 0;
+        for (i = 0; i < INPUT_DEPTH; i++) begin
+            shift_reg[i] <= 0;
+        end  
+    end else if (shift_enable) begin
+        out_data <= shift_reg[INPUT_DEPTH-1];
+        shift_reg[0] <= in_data;
+        for (i = 1; i < INPUT_DEPTH; i++) begin
+            shift_reg[i] <= shift_reg[i-1]; 
+        end
+    end
+end
+
+
+endmodule
--- bpTPU.srcs/sources_1/new/skew_buffer.sv
+++ bpTPU.srcs/sources_1/new/skew_buffer.sv
@@ -1,26 +1,35 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 01/14/2026 10:49:19 PM
-// Design Name: 
-// Module Name: skew_buffer
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+// Skew Buffer
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module skew_buffer(
+module skew_buffer # (
+    parameter int INPUT_WIDTH = 8,
+    parameter int N = 8
+) (
+    input logic clk,
+    input logic rst_n, 
+    input logic load_data,
+    input logic signed [INPUT_WIDTH-1:0] in_data [N-1:0],
+    output logic signed [INPUT_WIDTH-1:0] out_data [N-1:0]
+);
 
-    );
+genvar i, j;
+generate
+    for (i = 0; i < N; i++) begin : ROW
+        if (i == 0) begin
+            assign out_data[0] = in_data[0];
+        end else begin
+            // i shift registers
+            for (int j = 0; j < i; j++) begin
+                
+            end
+        end 
+    end
+
+endgenerate
+
+
+
 endmodule
