5f331cafd0822b8c4376b8dd3c78ae96b0c43065
benpegg23 <benpegg23@gmail.com>
2026-03-04 00:30:55 -0600

    added drawio diagram, started making changes to control unit

---
+++ bpTPU.srcs/sources_1/new/control.sv
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Control Unit
+// Manages instruction input
+// Manages data input/output
+// Manages control flow
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module control # (
+    parameter int INSTRUCTION_WIDTH = 4, 
+    parameter int WEIGHT_ADDR_WIDTH = 10,
+    parameter int DATA_WIDTH = 8,
+    parameter int ADDR_WIDTH = 10   // BRAM 
+) (
+    input logic clk,
+    input logic rst_n,
+    // instructions
+    input logic []
+    
+    /* 
+    inputs:
+    instructions from computer over UART
+    instruction ready/done
+    outputs:
+    select bit for input mux to activation skew buffer (mux between input bram and ReLU)
+    mmu control signals (switch load_weight on and off)
+    skew/deskew buffer control signals (load_data)
+    weight BRAM read/write control (enable and address)
+    output BRAM read/write control (enable, address, and data)
+    input BRAM read/write control (enable, address, and data)
+    */
+);
+endmodule
